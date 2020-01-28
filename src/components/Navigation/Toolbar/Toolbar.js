245d7639e99c607f1b344315338e90fed929df64
aarong31 <aarongonzales531@gmail.com>
2020-01-27 20:07:30 -0800

    Navigation/Toolbar/Sidedrawer update

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Menu from '../SideDrawer/Menu/Menu';
+//ss
 const toolbar = (props) => (
-    <header>
-        <div>MENU</div>
-        <div>LOGO</div>
-        <nav>
-            <ul></ul>
+    <header className={classes.Toolbar}>
+        <Menu clicked={props.menuToggleClicked} />
+        <Logo height="80%"/>
+        <nav className = {classes.DesktopOnly}>
+            <NavigationItems /> 
         </nav>
     </header>
 );
